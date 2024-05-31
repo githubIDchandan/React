72560212771457f9cfb97a7012abd97aef486431
chandanK <iamchandankumar02@gmail.com>
2024-05-31 12:04:54 +0530

    Lec4

---
+++ TalkIsCheapShowMetheCode4/app.js
@@ -0,0 +1,592 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const resList=[
+    {
+      "info": {
+        "id": "59631",
+        "name": "Subway",
+        "cloudinaryImageId": "63178e3e64d503a479f2a2048a474552",
+        "locality": "Kharar Road",
+        "areaName": "Shivalik City",
+        "costForTwo": "₹350 for two",
+        "cuisines": [
+          "Healthy Food",
+          "Salads",
+          "Snacks",
+          "Desserts",
+          "Beverages"
+        ],
+        "avgRating": 4.3,
+        "parentId": "2",
+        "avgRatingString": "4.3",
+        "totalRatingsString": "5K+",
+        "sla": {
+          "deliveryTime": 24,
+          "lastMileTravel": 2.6,
+          "serviceability": "SERVICEABLE",
+          "slaString": "20-25 mins",
+          "lastMileTravelString": "2.6 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-05-31 23:00:00",
+          "opened": true
+        },
+        "badges": {
+          "imageBadges": [
+            {
+              "imageId": "Rxawards/_CATEGORY-Sandwiches.png",
+              "description": "Delivery!"
+            }
+          ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "description": "Delivery!",
+                    "imageId": "Rxawards/_CATEGORY-Sandwiches.png"
+                  }
+                }
+              ]
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "₹100 OFF",
+          "subHeader": "ABOVE ₹599",
+          "discountTag": "FLAT DEAL"
+        },
+        "loyaltyDiscoverPresentationInfo": {
+          "logoCtx": {
+            "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+          },
+          "freedelMessage": "FREE DELIVERY",
+          "badgeType": "BADGE_TYPE_ONE_LITE"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/subway-kharar-road-shivalik-city-chandigarh-59631",
+        "type": "WEBLINK"
+      }
+    },
+    {
+      "info": {
+        "id": "772027",
+        "name": "McDonald's",
+        "cloudinaryImageId": "535fc9f9c135f7982317bbb6a64a1ffc",
+        "locality": "Amayra Emporio",
+        "areaName": "Mohali",
+        "costForTwo": "₹400 for two",
+        "cuisines": [
+          "American",
+          "Fast Food"
+        ],
+        "avgRating": 4.4,
+        "parentId": "630",
+        "avgRatingString": "4.4",
+        "totalRatingsString": "500+",
+        "sla": {
+          "deliveryTime": 24,
+          "lastMileTravel": 3,
+          "serviceability": "SERVICEABLE",
+          "slaString": "20-25 mins",
+          "lastMileTravelString": "3.0 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-06-01 00:00:00",
+          "opened": true
+        },
+        "badges": {
+          "imageBadges": [
+            {
+              "imageId": "Rxawards/_CATEGORY-Burger.png",
+              "description": "Delivery!"
+            }
+          ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "description": "Delivery!",
+                    "imageId": "Rxawards/_CATEGORY-Burger.png"
+                  }
+                }
+              ]
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "20% OFF",
+          "subHeader": "UPTO ₹50"
+        },
+        "loyaltyDiscoverPresentationInfo": {
+          "logoCtx": {
+            "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+          },
+          "freedelMessage": "FREE DELIVERY",
+          "badgeType": "BADGE_TYPE_ONE_LITE"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/mcdonalds-amayra-emporio-mohali-chandigarh-772027",
+        "type": "WEBLINK"
+      }
+    },
+    {
+      "info": {
+        "id": "707282",
+        "name": "Starbucks Coffee",
+        "cloudinaryImageId": "2418209798927d733a50f5d2ebcc2aee",
+        "locality": "Nirwana Square",
+        "areaName": "Chandigarh Ropar Road",
+        "costForTwo": "₹400 for two",
+        "cuisines": [
+          "Beverages",
+          "Cafe",
+          "Snacks",
+          "Desserts",
+          "Bakery",
+          "Ice Cream"
+        ],
+        "avgRating": 4.6,
+        "parentId": "195515",
+        "avgRatingString": "4.6",
+        "totalRatingsString": "100+",
+        "sla": {
+          "deliveryTime": 23,
+          "lastMileTravel": 3,
+          "serviceability": "SERVICEABLE",
+          "slaString": "20-25 mins",
+          "lastMileTravelString": "3.0 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-05-31 23:30:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "20% OFF",
+          "subHeader": "UPTO ₹50"
+        },
+        "loyaltyDiscoverPresentationInfo": {
+          "logoCtx": {
+            "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+          },
+          "freedelMessage": "FREE DELIVERY",
+          "badgeType": "BADGE_TYPE_ONE_LITE"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/starbucks-coffee-nirwana-square-chandigarh-ropar-road-chandigarh-707282",
+        "type": "WEBLINK"
+      }
+    },
+    {
+      "info": {
+        "id": "240666",
+        "name": "Burger King",
+        "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+        "locality": "SAS Nagar",
+        "areaName": "Greater Mohali",
+        "costForTwo": "₹350 for two",
+        "cuisines": [
+          "Burgers",
+          "American"
+        ],
+        "avgRating": 4.4,
+        "parentId": "166",
+        "avgRatingString": "4.4",
+        "totalRatingsString": "5K+",
+        "sla": {
+          "deliveryTime": 27,
+          "lastMileTravel": 5,
+          "serviceability": "SERVICEABLE",
+          "slaString": "25-30 mins",
+          "lastMileTravelString": "5.0 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-06-01 06:00:00",
+          "opened": true
+        },
+        "badges": {
+          "imageBadges": [
+            {
+              "imageId": "Rxawards/_CATEGORY-Burger.png",
+              "description": "Delivery!"
+            }
+          ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "description": "Delivery!",
+                    "imageId": "Rxawards/_CATEGORY-Burger.png"
+                  }
+                }
+              ]
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "ITEMS",
+          "subHeader": "AT ₹139"
+        },
+        "loyaltyDiscoverPresentationInfo": {
+          "logoCtx": {
+            "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+          },
+          "freedelMessage": "FREE DELIVERY",
+          "badgeType": "BADGE_TYPE_ONE_LITE"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/burger-king-sas-nagar-greater-mohali-chandigarh-240666",
+        "type": "WEBLINK"
+      }
+    },
+    {
+      "info": {
+        "id": "615795",
+        "name": "EatFit - Desi Meals, Burgers & More",
+        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/4/2/b83bc9a8-67dd-4e65-8738-47d72a7d9507_615795.JPG",
+        "locality": "SECTOR 125",
+        "areaName": "SUNNY ENCLAVE KHARAR MOHALI",
+        "costForTwo": "₹270 for two",
+        "cuisines": [
+          "Chinese",
+          "Healthy Food",
+          "Tandoor",
+          "Pizzas",
+          "North Indian",
+          "Thalis",
+          "Biryani"
+        ],
+        "avgRating": 4.2,
+        "parentId": "76139",
+        "avgRatingString": "4.2",
+        "totalRatingsString": "500+",
+        "sla": {
+          "deliveryTime": 21,
+          "lastMileTravel": 3,
+          "serviceability": "SERVICEABLE",
+          "slaString": "20-25 mins",
+          "lastMileTravelString": "3.0 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-06-01 01:00:00",
+          "opened": true
+        },
+        "badges": {
+          "imageBadges": [
+            {
+              "imageId": "Rxawards/_CATEGORY-Guiltfree.png",
+              "description": "Delivery!"
+            }
+          ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "description": "Delivery!",
+                    "imageId": "Rxawards/_CATEGORY-Guiltfree.png"
+                  }
+                }
+              ]
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "50% OFF",
+          "subHeader": "UPTO ₹100"
+        },
+        "loyaltyDiscoverPresentationInfo": {
+          "logoCtx": {
+            "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+          },
+          "freedelMessage": "FREE DELIVERY",
+          "badgeType": "BADGE_TYPE_ONE_LITE"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/eatfit-desi-meals-burgers-and-more-sector-125-sunny-enclave-kharar-mohali-chandigarh-615795",
+        "type": "WEBLINK"
+      }
+    }
+]
+
+const Headers=()=>{
+    return (
+        <div className="parentHeader">
+            <div>
+                <img className="imgLogo" src="https://marketplace.canva.com/EAFowsrK6x8/1/0/1600w/canva-red-and-yellow-catering-flat-illustrative-food-place-logo-rYbQJ_qtaz8.jpg"></img>
+            </div>
+            <div className="headerItem">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const RestaurantCard=(props)=>{
+    // console.log(props)
+    // const {menu}=props
+    // console.log(menu)
+    const {resData}=props;
+    console.log(resData)
+    return (
+        <div className="res-card">
+            <div>
+                <img className="foodimg" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/d0450ce1a6ba19ea60cd724471ed54a8"></img>
+            </div>
+            <div className="foodname">
+                <h2 className="fname">{resData.info.name}</h2>
+                <h3 className="fname">{resData.info.cuisines.join(", ")}</h3>
+                <h4 className="fname">{resData.info.avgRating} star</h4>
+                <h3 className="fname">{resData.info.sla.deliveryTime}{" "}mins</h3>
+            </div>
+        </div>
+    )
+}
+// const objlist={
+//     name: "kfc",
+//     price: 200
+// }
+const Body=()=>{
+    return(
+       <div className="parentBody">
+        <div>
+            <input className="foodseacrh" placeholder="Search Your Food"></input>
+            <button className="btn">Seacrh</button>
+        </div>
+        <div className="res-container">
+          {/* <RestaurantCard menu={objlist}/> */}
+          <RestaurantCard resData={resList[2]}/>
+          {
+            resList.map((item)=>{
+                return(
+                    <RestaurantCard resData={item}/>
+                )
+            })
+          }
+          
+        </div>
+       </div>
+    )
+}
+
+const Footer=()=>{
+    return (
+        <div className="parentFooter">
+           <div>
+             <h2 className="f1">A FOOD ORDERING COMPANY</h2>
+           </div>
+           <div className="privacyItem">
+              <ul>
+                 <li>Privacy Policy</li>
+                 <li>Careers</li>
+                 <li>Team</li>
+                 <li>Cookie Policy</li>
+              </ul>
+           </div>
+           <div className="f2">
+              <h3>Made with {"❤️"} india</h3>
+           </div>
+        </div>
+    )
+}
+
+
+const AppLayout=()=>{
+    return (
+        <div>
+        <Headers/>
+        <Body/>
+        <Footer/>
+        </div>
+    )
+}
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
